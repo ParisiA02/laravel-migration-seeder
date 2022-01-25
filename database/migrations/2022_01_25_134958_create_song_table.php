d60d8e3f2c8c3e8ae96c4c56fc997db4b26b2e78
Andrea Parisi <andreaparisi4502@gmail.com>
2022-01-25 14:57:50 +0100

    commit ex

---
+++ database/migrations/2022_01_25_134958_create_song_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('song', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name",20);
+            $table->float("duration");
+            $table->integer("reproductions");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('song');
+    }
+}
